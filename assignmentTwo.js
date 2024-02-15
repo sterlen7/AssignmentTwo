c3f27b45560bcb6046e8ed2b3fabbb5d5c10c0f1
sterlen7 <infosterlen@gmail.com>
2024-02-15 16:45:52 +0000

    Create assignmentTwo.js

---
+++ assignmentTwo.js
@@ -0,0 +1,25 @@
+//1 spread function
+
+//2 object 
+const person = {
+    Name: 'Lurstlove',
+    age: 23,
+    occupation: 'Plumber'
+}
+
+console.log(person)
+
+//3tenary operator
+
+function isEvenNumber(number) {
+    return (number % 2 == 0) ? 'Number is even' : 'Number is odd'
+}
+
+//test 
+const test1 = 3
+console.log(isEvenNumber(test1))
+
+const test2 = 100
+console.log(isEvenNumber(test2))
+
+//4
