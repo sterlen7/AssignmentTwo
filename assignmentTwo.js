3c0fd28347fee4b9239a8b603bcde61ebddc024c
sterlen7 <infosterlen@gmail.com>
2024-02-16 10:08:55 +0000

    Update assignmentTwo.js

--- assignmentTwo.js
+++ assignmentTwo.js
@@ -22,4 +22,52 @@ console.log(isEvenNumber(test1))
 const test2 = 100
 console.log(isEvenNumber(test2))
 
-//4
+//4map  function
+
+//5 first letter
+
+//6 reduce function
+
+//7 rest operator
+
+//8 the this keyword 
+
+function userMe(name, age, mass) {
+    this.name = name
+    this.age = age
+    this.mass = mass
+
+    this.greet = () => {
+        console.log(`welcome back ${this.name},your mass is ${this.mass} and your age ${this.age} `)
+    }
+}
+const userA = new userMe('Sterlen', 30, '50kg')
+
+userA.greet();
+
+//9 object 
+
+const porsche = {
+    model: '911',
+    make: 'GTR',
+    year: 2022,
+}
+
+console.log(porsche);
+
+//10 spread array
+
+// 11 tenary possitive 
+
+function checkNumber(number) {
+    return (number < 0) ? 'The number is negative' : 'The number is positive'
+}
+
+const hisNumber = -1
+
+console.log(checkNumber(hisNumber))
+
+
+//12 map concept
+
+//13 
